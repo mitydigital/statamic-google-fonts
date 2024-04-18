7b829a584a5c61cc31ae13f4e0991dd920f69dda
Duncan McClean <duncan@duncanmcclean.com>
2024-04-18 16:07:30 +0100

    Use Statamic's `AddonTestCase`

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,45 +4,16 @@ namespace MityDigital\StatamicGoogleFonts\Tests;
 
 use Illuminate\Filesystem\FilesystemManager;
 use Illuminate\Foundation\Application;
-use Illuminate\Contracts\Filesystem\Filesystem;
-use Illuminate\Support\Facades\Storage;
 use MityDigital\StatamicGoogleFonts\ServiceProvider;
-use Orchestra\Testbench\TestCase as OrchestraTestCase;
 use Spatie\GoogleFonts\GoogleFonts;
-use Spatie\GoogleFonts\GoogleFontsServiceProvider;
-use Statamic\Providers\StatamicServiceProvider;
-use Statamic\Statamic;use Statamic\Extend\Manifest;
+use Statamic\Testing\AddonTestCase;
 
-abstract class TestCase extends OrchestraTestCase
+abstract class TestCase extends AddonTestCase
 {
-    //
-
-    public function disk(): Filesystem
-    {
-        $diskName = config('google-fonts.disk');
-
-        return Storage::disk($diskName);
-    }
-
-    protected function getPackageProviders($app)
-    {
-        return [
-            StatamicServiceProvider::class,
-            //GoogleFontsServiceProvider::class,
-            ServiceProvider::class,
-        ];
-    }
-
-    protected function getPackageAliases($app)
-    {
-        return [
-            'Statamic' => Statamic::class,
-        ];
-    }
+    protected string $addonServiceProvider = ServiceProvider::class;
 
     protected function getEnvironmentSetUp($app)
     {
-
         parent::getEnvironmentSetUp($app);
 
         config()->set('filesystems.disks', array_merge(
@@ -59,7 +30,7 @@ abstract class TestCase extends OrchestraTestCase
 
         config()->set('google-fonts.fonts', [
             'default' => 'default-font-url',
-            'code' => 'code-font-url'
+            'code' => 'code-font-url',
         ]);
         config()->set('google-fonts.disk', 'fonts');
         config()->set('google-fonts.path', '');
@@ -67,7 +38,6 @@ abstract class TestCase extends OrchestraTestCase
         config()->set('google-fonts.fallback', false);
         config()->set('google-fonts.user_agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15');
         config()->set('google-fonts.preload', false);
-
 
         $app->singleton(GoogleFonts::class, function (Application $app) {
             return new GoogleFonts(
@@ -80,16 +50,5 @@ abstract class TestCase extends OrchestraTestCase
                 preload: config('google-fonts.preload'),
             );
         });
-
-        $app->make(Manifest::class)->manifest = [
-            /*'spatie/laravel-google-fonts' => [
-                'id'        => 'spatie/laravel-google-fonts',
-                'namespace' => 'Spatie\\GoogleFonts',
-            ],*/
-            'mitydigital/statamic-google-fonts' => [
-                'id'        => 'mitydigital/statamic-google-fonts',
-                'namespace' => 'MityDigital\\StatamicGoogleFonts',
-            ],
-        ];
     }
 }
